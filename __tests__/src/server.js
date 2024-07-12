b1469183eeb54d01faf0fb5b5675c91d4463cc06
OzhoginCode <danozh.b@gmail.com>
2024-07-12 18:03:48 +0300

    write tests

---
+++ __tests__/src/server.js
@@ -0,0 +1,69 @@
+import { http, HttpResponse } from 'msw';
+import { setupServer } from 'msw/node';
+
+const teams = Array.from({ length: 20 }, (_, i) => ({
+  name: `команда ${i + 1}`,
+  id: i + 1,
+  crestUrl: `/some-img-${i + 1}`,
+}));
+
+const competitions = Array.from({ length: 20 }, (_, i) => ({
+  name: `лига ${i + 1}`,
+  id: i + 1,
+  area: {
+    name: `страна ${i + 1}`,
+  },
+}));
+
+const matches = Array.from({ length: 20 }, (_, i) => ({
+  id: i + 1,
+  utcDate: i === 1 ? '2025-01-01T12:00:00.850Z' : '2024-01-01T12:00:00.850Z',
+  status: 'SCHEDULED',
+  homeTeam: { name: `Home Team ${i + 1}` },
+  awayTeam: { name: `Away Team ${i + 1}` },
+  score: {
+    fullTime: { homeTeam: i % 2 === 0 ? 1 : 2, awayTeam: i % 2 === 0 ? 2 : 1 },
+    extraTime: { homeTeam: i === 1 ? 1 : null, awayTeam: i === 1 ? 1 : null },
+    penalties: { homeTeam: i === 1 ? 2 : null, awayTeam: i === 1 ? 2 : null },
+  },
+}));
+
+const server = setupServer(
+  http.get('/api/teams', () => HttpResponse.json({ teams })),
+  http.get('/api/teams/:id', ({ params }) => HttpResponse.json({ name: `команда ${params.id}` })),
+  http.get('/api/teams/:id/matches', ({ request }) => {
+    const url = new URL(request.url);
+    const dateFrom = url.searchParams.get('dateFrom');
+    const dateTo = url.searchParams.get('dateTo');
+    let filteredMatches = matches;
+
+    if (dateFrom && dateTo) {
+      const fromDate = new Date(dateFrom);
+      const toDate = new Date(dateTo);
+      filteredMatches = matches.filter((match) => {
+        const matchDate = new Date(match.utcDate);
+        return matchDate >= fromDate && matchDate <= toDate;
+      });
+    }
+    return HttpResponse.json({ matches: filteredMatches });
+  }),
+  http.get('/api/competitions', () => HttpResponse.json({ competitions })),
+  http.get('/api/competitions/:id/matches', ({ params, request }) => {
+    const url = new URL(request.url);
+    const dateFrom = url.searchParams.get('dateFrom');
+    const dateTo = url.searchParams.get('dateTo');
+    let filteredMatches = matches;
+
+    if (dateFrom && dateTo) {
+      const fromDate = new Date(dateFrom);
+      const toDate = new Date(dateTo);
+      filteredMatches = matches.filter((match) => {
+        const matchDate = new Date(match.utcDate);
+        return matchDate >= fromDate && matchDate <= toDate;
+      });
+    }
+    return HttpResponse.json({ competition: { name: `Лига ${params.id}` }, matches: filteredMatches });
+  }),
+);
+
+export default server;
---
+++ __tests__/teamCalendarPage.test.jsx
@@ -0,0 +1,93 @@
+import {
+  expect, test, beforeAll, afterEach, afterAll,
+} from 'vitest';
+import React from 'react';
+import { Route, Routes, MemoryRouter } from 'react-router-dom';
+import {
+  render, screen, fireEvent, within, waitFor,
+} from '@testing-library/react';
+
+import server from './src/server.js';
+
+import TeamCalendar from '../src/pages/TeamCalendar.jsx';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test('teams table render', async () => {
+  render(
+    <MemoryRouter initialEntries={['/team/1']}>
+      <Routes>
+        <Route path="/team/:id" element={<TeamCalendar />} />
+      </Routes>
+    </MemoryRouter>,
+  );
+
+  const team1 = await screen.findByText('Home Team 1');
+  const team1El = team1.closest('tr');
+  await within(team1El).findByText('01.01.2024');
+  await within(team1El).findByText(/запланирован/i);
+  await within(team1El).findByText('Away Team 1');
+  await within(team1El).findByText('1:2');
+
+  const team2 = await screen.findByText('Home Team 2');
+  const team2El = team2.closest('tr');
+  await within(team2El).findByText('01.01.2025');
+  await within(team2El).findByText(/запланирован/i);
+  await within(team2El).findByText('Away Team 2');
+  await within(team2El).findByText('2:1');
+  await within(team2El).findByText('(1:1) (2:2)');
+});
+
+test('date filtering', async () => {
+  render(
+    <MemoryRouter initialEntries={['/team/1']}>
+      <Routes>
+        <Route path="/team/:id" element={<TeamCalendar />} />
+      </Routes>
+    </MemoryRouter>,
+  );
+
+  const dateInput = await screen.findByRole('presentation');
+  fireEvent.change(dateInput, { target: { value: '01/01/2025 - 02/01/2025' } });
+
+  await waitFor(
+    () => {
+      const team1 = screen.queryByText('Home Team 1');
+      return team1 === null;
+    },
+  );
+
+  const team1 = screen.queryByText('Home Team 1');
+  expect(team1).toBeNull();
+
+  const team2 = screen.queryByText('Home Team 2');
+  const team2El = team2.closest('tr');
+  await within(team2El).findByText('01.01.2025');
+});
+
+test('pagination', async () => {
+  render(
+    <MemoryRouter initialEntries={['/team/1']}>
+      <Routes>
+        <Route path="/team/:id" element={<TeamCalendar />} />
+      </Routes>
+    </MemoryRouter>,
+  );
+
+  await screen.findByText('Home Team 1');
+  await screen.findByText('Home Team 10');
+  const team11 = screen.queryByText('Home Team 11');
+  expect(team11).toBeNull();
+
+  const paginationNav = screen.getByLabelText('Pagination');
+  const secondPageButton = within(paginationNav).getByText('2');
+
+  fireEvent.click(secondPageButton);
+
+  await screen.findByText('Home Team 11');
+  await screen.findByText('Home Team 20');
+  const team1 = screen.queryByText('Home Team 1');
+  expect(team1).toBeNull();
+});
