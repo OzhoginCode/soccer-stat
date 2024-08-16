006a10054bb625377f26c16ffc8ad97f0765035a
OzhoginCode <danozh.b@gmail.com>
2024-08-16 12:35:02 +0300

    add types for useErrorHandling.ts

--- src/hooks/useErrorHandling.ts
+++ src/hooks/useErrorHandling.ts
@@ -1,27 +1,45 @@
 import { useState, useEffect, useCallback } from 'react';
-import { useQueryClient } from '@tanstack/react-query';
+import { useQueryClient, QueryKey } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
-const useErrorHandling = (error, fetchStatus, queryKey) => {
+type ErrorType = AxiosError | Error | null;
+
+const useErrorHandling = (error: ErrorType, fetchStatus: string, queryKey: QueryKey) => {
   const [modalOpen, setModalOpen] = useState(false);
   const [reloadTime, setReloadTime] = useState(0);
-  const [errorType, setErrorType] = useState('');
+  const [errorType, setErrorType] = useState(0);
 
   const queryClient = useQueryClient();
 
-  const reload = useCallback(() => {
-    queryClient.invalidateQueries(queryKey);
+  const reload = useCallback(async () => {
+    await queryClient.invalidateQueries({ queryKey });
     setModalOpen(false);
   }, [queryClient]); // eslint-disable-line react-hooks/exhaustive-deps
 
   useEffect(() => {
-    if (!error || fetchStatus === 'fetching') return;
+    // Ниже guard expressions (if-ы) для правильной обработки разных типов ошибок
+    if (!error) return;
+    if (fetchStatus === 'fetching') return;
+
+    if (!('response' in error)) {
+      setErrorType(0);
+      setModalOpen(true);
+      return;
+    }
+
+    if (!error.response) {
+      setErrorType(0);
+      setModalOpen(true);
+      return;
+    }
+
     const { status, data } = error.response;
     setErrorType(status);
     setModalOpen(true);
 
     if (status === 429) {
-      const { message } = data;
-      const waitTime = parseInt(message.match(/\d+/)[0], 10);
+      const { message } = data as { message: string };
+      const waitTime = parseInt(message.match(/\d+/)![0], 10);
       setReloadTime(waitTime);
 
       const timer = setTimeout(reload, waitTime * 1000);
